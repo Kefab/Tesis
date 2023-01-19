ce8bf3f19718ee9aea026a3144ebce7df65474ab
kefab <ksalazar@emagic.fin.ec>
2023-01-19 10:39:34 -0500

    Final server

--- scripts/dbOperations.js
+++ scripts/dbOperations.js
@@ -1,9 +1,17 @@
 const mysql = require("mysql");
 
 function getUser(connection, user, callback) {
-  let partial_query =
-    "SELECT * FROM login_face.user WHERE username = ? AND password = ?;";
-  let query = mysql.format(partial_query, [user.username, user.password]);
+  let partial_query = "SELECT * FROM login_face.user WHERE username = ?;";
+  let query = mysql.format(partial_query, [user.username]);
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+function getUserById(connection, user, callback) {
+  let partial_query = "SELECT * FROM login_face.user WHERE id_user = ?;";
+  let query = mysql.format(partial_query, [user.id]);
   connection.query(query, function (err, result) {
     if (err) throw err;
     callback(result);
@@ -12,12 +20,64 @@ function getUser(connection, user, callback) {
 
 function insertUser(connection, user, callback) {
   let partial_query =
-    "INSERT INTO login_face.user (username, password) VALUES (?, ?);";
-  let query = mysql.format(partial_query, [user.username, user.password]);
+    "INSERT INTO login_face.user (username, password,phone_number) VALUES (?, ?, ?);";
+  let query = mysql.format(partial_query, [
+    user.username,
+    user.password,
+    user.phoneNumber,
+  ]);
   connection.query(query, function (err, result) {
     if (err) throw err;
     callback(result);
   });
 }
 
-module.exports = { getUser,insertUser };
+function increment_attempt(connection, user, callback) {
+  let partial_query =
+    "UPDATE login_face.user SET login_attempts = ? WHERE id_user = ?;";
+  let query = mysql.format(partial_query, [user.attempt, user.id]);
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+function uploadImage(connection, user, callback) {
+  let partial_query =
+    "UPDATE login_face.user SET img_uploaded = 1 WHERE id_user = ?;";
+  let query = mysql.format(partial_query, [user.id]);
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+function uploadAcces(connection, user, callback) {
+  let partial_query =
+    "UPDATE login_face.user SET have_acces = 1 WHERE id_user = ?;";
+  let query = mysql.format(partial_query, [user.id]);
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+function increment_recognition(connection, user, callback) {
+  let partial_query =
+    "UPDATE login_face.user SET recognition_attempts = ? WHERE id_user = ?;";
+  let query = mysql.format(partial_query, [user.attempt, user.id]);
+  connection.query(query, function (err, result) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+module.exports = {
+  getUser,
+  insertUser,
+  increment_attempt,
+  getUserById,
+  uploadImage,
+  uploadAcces,
+  increment_recognition,
+};
--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const path = require("path");
 const { get } = require("request");
 var mysql = require("mysql");
-const { getUser, insertUser } = require("./scripts/dbOperations.js");
+const {
+  getUser,
+  insertUser,
+  increment_attempt,
+  getUserById,
+  uploadImage,
+  uploadAcces,
+  increment_recognition,
+} = require("./scripts/dbOperations.js");
 var cors = require("cors");
 const multer = require("multer");
 
@@ -35,20 +43,36 @@ app.use(express.static(path.join(__dirname, "./weights")));
 app.use(express.static(path.join(__dirname, "./dist")));
 
 app.post("/login", (req, res) => {
-  console.log("Entro");
   const username = req.body.username;
   const password = req.body.password;
   getUser(con, { username, password }, (result) => {
-    if (result.length == 1) {
-      res.status(200).json({ isUser: true });
+    if (result.length <= 0) {
+      res.status(201).send({ isUser: false, id: -2 });
+    } else if (result[0].password === password) {
+      res.status(200).send({ id: result[0].id_user, isUser: true });
+    } else if (Number(result[0].login_attempts) >= 3) {
+      res.status(202).send({ isUser: false, id: -1 });
     } else {
-      res.status(201).json({ isUser: false });
+      increment_attempt(
+        con,
+        {
+          id: result[0].id_user,
+          attempt: Number(result[0].login_attempts) + 1,
+        },
+        (result) => {
+          console.log("incremento");
+        }
+      );
+      res.status(201).send({ isUser: false, id: -2 });
     }
   });
 });
 
 app.post("/uploadImage", upload.single("image"), (req, res) => {
-  console.log(req.file);
+  id = req.body.id;
+  uploadImage(con, { id }, (result) => {
+    console.log("updated");
+  });
   res.send("ok");
 });
 
@@ -61,9 +85,55 @@ app.post("/getImage", (req, res) => {
 app.post("/registUser", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  insertUser(con, { username, password }, (result) => {
-    res.status(200).end("ok");
+  const phoneNumber = req.body.phoneNumber;
+  insertUser(con, { username, password, phoneNumber }, (result) => {
+    console.log(result.insertId);
+    res.status(200).send({ id: result.insertId });
   });
+});
+
+app.post("/uploaded", (req, res) => {
+  const id = req.body.id;
+  getUserById(con, { id }, (result) => {
+    res.send({ uploaded: result[0].img_uploaded });
+  });
+});
+
+app.post("/loged", (req, res) => {
+  const id = req.body.id;
+  console.log(id);
+  getUserById(con, { id }, (result) => {
+    res.send({ uploaded: result[0].have_acces });
+  });
+});
+
+app.post("/acces", (req, res) => {
+  acces = req.body.acces;
+  id = req.body.id;
+  if (acces == 1) {
+    uploadAcces(con, [id], (result) => {
+      console.log("updated");
+    });
+    res.status(200).send({ message: "OK" });
+  } else {
+    getUserById(con, { id }, (result) => {
+      if (result[0].recognition_attempts >= 3) {
+        res.status(201).send({ message: "UP" });
+      } else {
+        increment_recognition(
+          con,
+          {
+            id: result[0].id_user,
+            attempt: Number(result[0].recognition_attempts) + 1,
+          },
+          (result) => {
+            console.log("incremento");
+          }
+        );
+        res.status(202).send({ message: "Fallo" });
+      }
+    });
+  }
 });
 
 app.listen(3000, () => console.log("Listening on port 3000!"));
